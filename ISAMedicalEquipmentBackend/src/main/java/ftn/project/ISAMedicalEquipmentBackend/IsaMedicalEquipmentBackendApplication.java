19fc7728d25df78708b01702af6a31d2df53b26d
Darijan Mićić <darijan.micic10@gmail.com>
2023-11-08 22:31:00 +0100

    chore: create backend java spring boot project

---
+++ ISAMedicalEquipmentBackend/src/main/java/ftn/project/ISAMedicalEquipmentBackend/IsaMedicalEquipmentBackendApplication.java
@@ -0,0 +1,11 @@
+package ftn.project.ISAMedicalEquipmentBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaMedicalEquipmentBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IsaMedicalEquipmentBackendApplication.class, args);
+	}
+}
---
+++ ISAMedicalEquipmentBackend/src/test/java/ftn/project/ISAMedicalEquipmentBackend/IsaMedicalEquipmentBackendApplicationTests.java
@@ -0,0 +1,10 @@
+package ftn.project.ISAMedicalEquipmentBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaMedicalEquipmentBackendApplicationTests {
+	@Test
+	void contextLoads() {}
+}
