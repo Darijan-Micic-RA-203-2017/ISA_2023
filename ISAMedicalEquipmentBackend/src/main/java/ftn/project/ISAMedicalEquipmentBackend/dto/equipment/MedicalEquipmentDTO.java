a6edaa69eba9cb30bf82e64c26138d1308b33d9f
Darijan Mićić <darijan.micic10@gmail.com>
2023-11-19 23:49:55 +0100

    feat(3.1): create dtos of equipment & its type

---
+++ ISAMedicalEquipmentBackend/src/main/java/ftn/project/ISAMedicalEquipmentBackend/dto/equipment/MedicalEquipmentDTO.java
@@ -0,0 +1,60 @@
+package ftn.project.ISAMedicalEquipmentBackend.dto.equipment;
+
+public class MedicalEquipmentDTO {
+	private long id;
+	private TypeOfMedicalEquipmentDTO type;
+	private String name;
+	private double price;
+	private int amount;
+	
+	public MedicalEquipmentDTO() {}
+	
+	public MedicalEquipmentDTO(long id, TypeOfMedicalEquipmentDTO type, String name, double price, 
+			int amount) {
+		this.id = id;
+		this.type = type;
+		this.name = name;
+		this.price = price;
+		this.amount = amount;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public TypeOfMedicalEquipmentDTO getType() {
+		return type;
+	}
+	
+	public void setType(TypeOfMedicalEquipmentDTO type) {
+		this.type = type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public int getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+}
