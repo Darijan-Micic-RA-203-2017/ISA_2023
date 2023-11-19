cf2d0acad16b3830d6731ac9f28e9b863131522e
Darijan Mićić <darijan.micic10@gmail.com>
2023-11-19 20:18:16 +0100

    feat(3.2): provide services in app module file
    Reference: https://github.com/ivana-k/isa-vezbe/tree/main/Vezbe05/spring-security-example

--- ISAMedicalEquipmentFrontend/src/app/app.module.ts
+++ ISAMedicalEquipmentFrontend/src/app/app.module.ts
@@ -1,8 +1,14 @@
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TokenInterceptor } from './interceptor/token-interceptor';
+import { AuthService } from './services/auth/auth.service';
+import { UserService } from './services/user/user.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +16,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    JwtHelperService,
+    AuthService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
