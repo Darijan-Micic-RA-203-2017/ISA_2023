c81e7cd3d37863d9093eb20edd263ee924bcd430
Darijan Mićić <darijan.micic10@gmail.com>
2023-11-19 20:12:14 +0100

    feat(3.2): create registering & activation service
    Reference: https://github.com/ivana-k/isa-vezbe/tree/main/Vezbe05/spring-security-example

---
+++ ISAMedicalEquipmentFrontend/src/app/services/user/user.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { ProcurementManagerRegistrationReq } from 'src/app/domain/procurement-manager-registration-req';
+import { UserCodeWrapper } from 'src/app/domain/user-code-wrapper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private usersControllerURL: string = 'http://localhost:8080/users';
+  private procurementManagerRegistrationURL: string = 
+      this.usersControllerURL.concat('/register-as-a-procurement-manager');
+  private accountActivationURL: string = 
+      this.usersControllerURL.concat('/activate-account');
+  
+  constructor(private httpClient: HttpClient) { }
+  
+  registerAsAProcurementManager(procurementManagerRegistrationReq: ProcurementManagerRegistrationReq): Observable<any> {
+    const registrationHeaders = new HttpHeaders({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    });
+
+    return this.httpClient.post(this.procurementManagerRegistrationURL, 
+        JSON.stringify(procurementManagerRegistrationReq), { headers: registrationHeaders });
+  }
+
+  activateAccount(codeOfNewRegisteredUserWrapper: UserCodeWrapper): Observable<any> {
+    const accountActivationHeaders = new HttpHeaders({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    });
+
+    return this.httpClient.post(this.accountActivationURL, JSON.stringify(codeOfNewRegisteredUserWrapper), 
+        { headers: accountActivationHeaders });
+  }
+}
