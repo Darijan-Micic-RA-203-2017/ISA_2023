d9c0d3148a4e5c63fc2be06dc1c073791250b22d
Darijan Mićić <darijan.micic10@gmail.com>
2023-12-13 21:03:19 +0100

    feat(3.6): create only authenticated users guard
    Reference: https://github.com/ivana-k/isa-vezbe/tree/main/Vezbe05/spring-security-front-app

---
+++ ISAMedicalEquipmentFrontend/src/app/guards/only-authenticated-users/only-authenticated-users.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from 'src/app/services/auth/auth.service';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OnlyAuthenticatedUsersGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router, private snackBar: MatSnackBar) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | 
+      Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (!this.authService.hasTokenExpired()) {
+      let userRole = localStorage.getItem('role');
+      if (userRole) {
+        return true;
+      }
+    }
+    
+    console.log('Guard "OnlyAuthenticatedUsersGuard" has denied activation of route "' + route.url.toString() + '"!');
+    this.router.navigateByUrl('/login').then(() => {
+      this.snackBar.open('Nemate pravo pristupa unetoj putanji!', 'Zatvori', { duration: 7500 });
+    });
+    
+    return false;
+  }
+}
