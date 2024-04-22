4a1794fbdb532670298d59fe5f4af673aea45f1a
Darijan Mićić <darijan.micic10@gmail.com>
2024-04-22 19:07:27 +0200

    fix: set true prime no. in equipment's hash code

--- ISAMedicalEquipmentBackend/src/main/java/ftn/project/ISAMedicalEquipmentBackend/domain/equipment/MedicalEquipment.java
+++ ISAMedicalEquipmentBackend/src/main/java/ftn/project/ISAMedicalEquipmentBackend/domain/equipment/MedicalEquipment.java
@@ -99,7 +99,7 @@ public class MedicalEquipment {
 	
 	@Override
 	public int hashCode() {
-		final int prime = 51;
+		final int prime = 91;
 		int result = 1;
 		
 		result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
