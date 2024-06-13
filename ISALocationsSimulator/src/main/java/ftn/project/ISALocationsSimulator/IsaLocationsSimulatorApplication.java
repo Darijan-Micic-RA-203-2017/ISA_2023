19667a84765b4208ebec45bcea6aa61e3ac76a22
Darijan Mićić <darijan.micic10@gmail.com>
2024-06-13 21:34:04 +0200

    feat(5): create spring boot locations simulator

---
+++ ISALocationsSimulator/src/main/java/ftn/project/ISALocationsSimulator/IsaLocationsSimulatorApplication.java
@@ -0,0 +1,11 @@
+package ftn.project.ISALocationsSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaLocationsSimulatorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IsaLocationsSimulatorApplication.class, args);
+	}
+}
---
+++ ISALocationsSimulator/src/test/java/ftn/project/ISALocationsSimulator/IsaLocationsSimulatorApplicationTests.java
@@ -0,0 +1,10 @@
+package ftn.project.ISALocationsSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaLocationsSimulatorApplicationTests {
+	@Test
+	void contextLoads() {}
+}
